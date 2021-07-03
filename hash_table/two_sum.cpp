015cd1f81c329b610a88d8147da93defb3cf73d5
Brian Wu <sonny40920710@gmail.com>
2021-07-03 20:34:32 +0800

    Modify two_sum.cpp: implement a O(n) solution using a hash map

--- hash_table/two_sum.cpp
+++ hash_table/two_sum.cpp
@@ -1,3 +1,8 @@
+/*
+  https://leetcode.com/problems/two-sum/
+*/
+
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -5,7 +10,7 @@ using namespace std;
 class Solution {
  public:
   // O(n^2) naive solution
-  vector<int> twoSum1(vector<int>& nums, int target) {
+  vector<int> twoSum1(const vector<int>& nums, int target) {
     vector<int> answer(2);
     
     for (int i = 0; i < nums.size() - 1; ++i) {
@@ -21,5 +26,23 @@ class Solution {
     return answer;
   }
 
-  // O(n) solution
+  // O(n) solution using a hash map
+  vector<int> twoSum2(const vector<int>& nums, int target) {
+    vector<int> answer(2);
+    unordered_map<int, int> num_to_i;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      int num_to_find = target - nums[i];
+      // If the number is found in the hash map, set the answer
+      if (num_to_i.find(num_to_find) != num_to_i.end()) {
+        answer[0] = i;
+        answer[1] = num_to_i[num_to_find];
+        break;
+      }
+      // Else put the number and the index in the hash map
+      num_to_i[nums[i]] = i;
+    }
+
+    return answer;
+  }
 };
