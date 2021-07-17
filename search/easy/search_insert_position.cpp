5bf72ec7fbefd06ade2680c570d732a392e42bc9
Brian Wu <sonny40920710@gmail.com>
2021-07-18 00:00:29 +0800

    implement recursive version of binary insert

--- search/easy/search_insert_position.cpp
+++ search/easy/search_insert_position.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 class Solution {
  public:
-  int searchInsert(vector<int>& nums, int target) {
+  // Interative
+  int searchInsertIter(vector<int>& nums, int target) {
     int low = 0, high = nums.size() - 1;
     
     while (low <= high) {
@@ -19,5 +20,26 @@ class Solution {
     }
     
     return low;
+  }
+
+  // Recursive
+  int searchInsertRecur(vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+    return search(nums, low, high, target);
+  }
+  
+  int search(vector<int>& nums, int low, int high, int target) {
+    if (low > high) {
+      return low;
+    }
+    
+    int mid = low + (high - low) / 2;
+    if (nums[mid] == target) {
+      return mid;
+    } else if (target < nums[mid]) {
+      return search(nums, low, mid - 1, target);
+    } else {
+      return search(nums, mid + 1, high, target);
+    }
   }
 };
